7c05fdb3b90ebf5a3189ea7ccd78ff4dce88a4d5
bappyasif <bappyasif@hotmail.com>
2023-08-28 01:51:06 +0600

    adjusting skills ui view

--- src/components/Portfolio/Skills/index.tsx
+++ src/components/Portfolio/Skills/index.tsx
@@ -36,24 +36,69 @@ const ShowHighPercentilesSkills = ({ data }: MetricsData) => {
 }
 
 const ShowMetric = ({ ...item }: MetricProps) => {
-    const { name, text, percentile } = item;
+    // const { name, text, percentile } = item;
+    const { name, percentile } = item;
 
-    const {currPercentile, handleBegin, handleStop} = useHandlePercentileCount(percentile)
-    
+    const { currPercentile, handleBegin, handleStop } = useHandlePercentileCount(percentile)
+
     return (
         <div
-            className="xxs:w-3/6 sm:w-1/4 lg:w-1/4 mx-auto bg-slate-600 rounded-2xl py-2"
+            className="xxs:w-3/6 sm:w-1/4 lg:w-1/4 h-fit mx-auto bg-slate-600 rounded-2xl py-2 relative"
             // onClick={handleToggle}
             onMouseEnter={handleBegin}
             onMouseLeave={handleStop}
         >
             <h2 className="text-2xl">{name}</h2>
             {/* <RadialProgressBar percentile={percentile} /> */}
-            <RadialProgressBar percentile={ currPercentile || percentile} />
-            <p>{text} *</p>
+            {/* <CurvedTextInCircle /> */}
+            <RadialProgressBar percentile={currPercentile || percentile} />
+            {/* <p>{text} *</p> */}
         </div>
     )
 }
+
+// const CurvedTextInCircle = () => {
+//     return (
+//         <svg className="absolute left-6" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width={260} height={200}>
+//             {/* <path id="curve" d="M 0 120 C 0 120, 130 0, 260 180"></path> */}
+//             {/* <path id="curve" d="M50,250c0-110.5,89.5-200,200-200s200,89.5,200,200s-89.5,200-200,200S50,360.5,50,250"></path> */}
+//             {/* <path id="curve" d="M0,60a60,60 0 1,0 120,0a60,60 0 1,0 -120,0" fill="none"></path> */}
+//             {/* <path id="curve" d="M16,60a44,44 0 1,0 88,0a44,44 0 1,0 -88,0" fill="none"></path> */}
+//             {/* <path id="curve" d="M2,62a60,60 0 1,0 120,0a60,60 0 1,0 -120,0" fill="none"></path> */}
+//             <path id="curve" d="M16,60a44,44 0 1,0 88,0a44,44 0 1,0 -88,0" fill="none"></path>
+//             <text className="font-mono tracking-widest fill-green-400 text-sm">
+//                 <textPath startOffset={"0%"} href="#curve">Proficiency*</textPath>
+//             </text>
+
+//             {/* <path d="M2,62a60,60 0 1,0 120,0a60,60 0 1,0 -120,0" id="textcircle" fill="none"></path>
+//             <text dy="70" textLength="1220">
+//                 <!-- textLength (essentially the circumference of the circle) is used as an alternative to letter-spacing for Firefox, which currently doesn't support letter-spacing for SVG -->
+//                 <textPath xlinkHref="#textcircle">Red Hot Chilli Peppers</textPath>
+//             </text> */}
+
+//         </svg>
+//     )
+// }
+
+// const CurvedTextInCircle = () => {
+//     return (
+//         <svg className="absolute" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 200 200">
+//             <title>Starbucks Coffee Logo</title>
+//             <defs>
+//                 <path d="M15,100a85,85 0 1,0 170,0a85,85 0 1,0 -170,0" id="coffeecircle" />
+//                 <path d="M100,37c34.8,0,63,28.2,63,63s-28.2,63-63,63s-63-28.2-63-63S65.2,37,100,37z" id="starbuckscircle" transform="rotate(-85 100 100)" />
+//             </defs>
+//             <circle cx="60" cy="60" r="60" fill="transparent" id="background" />
+//             <circle fill="none" stroke="#FFF" cx="60" cy="60" r="60" stroke-width="3" id="rim" />
+//             <text>
+//                 <textPath xlinkHref="#starbuckscircle">Starbucks</textPath>
+//             </text>
+//             <text dx="20" textLength="160" className="coffee">
+//                 <textPath xlinkHref="#coffeecircle">Coffee</textPath>
+//             </text>
+//         </svg>
+//     )
+// }
 
 /**
  * 
@@ -84,6 +129,7 @@ const RadialProgressBar = ({ percentile }: ProgressProps) => {
             width={radius * 2}
         >
             <circle
+                // id="textcircle"
                 stroke="white"
                 fill="transparent"
                 strokeWidth={stroke}
@@ -93,7 +139,15 @@ const RadialProgressBar = ({ percentile }: ProgressProps) => {
                 r={normalizedRadius}
                 cx={radius}
                 cy={radius}
-            />
+            >
+                {/* you can’t yet make text cling to a <circle> or <rect></rect> */}
+
+                {/* <text dx="50">
+                    <textPath xlinkHref="#textcircle">
+                        Red Hot Chilli Peppers
+                    </textPath>
+                </text> */}
+            </circle>
             <text className="text-2xl"
                 x="50%" y="50%" textAnchor="middle" fill="white" dy=".3em">{percentile}%
             </text>
@@ -102,7 +156,7 @@ const RadialProgressBar = ({ percentile }: ProgressProps) => {
 }
 
 const ShowRegulars = ({ data }: MetricsData) => {
-    const showMetrics = () => data.sort((a,b)=>a.percentile > b.percentile ? 1 : -1).map(item => <ShowSkillProgress name={item.name} percentile={item.percentile} text={item.text} key={item.name} />)
+    const showMetrics = () => data.sort((a, b) => a.percentile > b.percentile ? 1 : -1).map(item => <ShowSkillProgress name={item.name} percentile={item.percentile} text={item.text} key={item.name} />)
 
     return (
         <div
@@ -114,20 +168,21 @@ const ShowRegulars = ({ data }: MetricsData) => {
 }
 
 const ShowSkillProgress = ({ ...item }: MetricProps) => {
-    const { name, text, percentile } = item;
+    // const { name, text, percentile } = item;
+    const { name, percentile } = item;
 
-    const {currPercentile, handleBegin, handleStop} = useHandlePercentileCount(percentile)
+    const { currPercentile, handleBegin, handleStop } = useHandlePercentileCount(percentile)
 
     return (
-        <div 
+        <div
             className={`xxs:w-2/5 lg:w-1/3 flex flex-col justify-end gap-2`}
             onMouseEnter={handleBegin}
             onMouseLeave={handleStop}
         >
             <h2 className="text-xl">{name}</h2>
             {/* <LinearProgressBar percentile={percentile} /> */}
-            <LinearProgressBar percentile={ currPercentile || percentile} />
-            <p className="text-sm xxs:hidden lg:block">Current {text} *</p>
+            <LinearProgressBar percentile={currPercentile || percentile} />
+            {/* <p className="text-sm xxs:hidden lg:block">Current {text} *</p> */}
         </div>
     )
 }
@@ -138,12 +193,14 @@ type ProgressProps = {
 
 const LinearProgressBar = ({ percentile }: ProgressProps) => {
     return (
-        <div className='h-2 w-full bg-gray-300 rounded overflow-hidden'>
+        <div className='w-full bg-gray-300 rounded overflow-hidden'>
             <div
                 style={{ width: `${percentile}%` }}
-                className={`h-full ${percentile < 56 ? 'bg-red-600' : 'bg-green-600'
+                className={`h-5 ${percentile < 56 ? 'bg-red-600' : 'bg-green-600'
                     }`}
-            ></div>
+            >
+                <p className="text-sm h-fit">Profeciency*</p>
+            </div>
         </div>
     );
 };
